bc713e7a6eb6b239aaf1e50b01e8eae5ad3d28e3
vagrant <vagrant@localhost.localdomain>
2020-08-18 08:52:36 +0000

    [Fix]削除機能、投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,20 @@
 class BooksController < ApplicationController
-
+before_action :authenticate_user!
+before_action :correct_user, only: [:edit, :update]
   def show
+    @book_new = Book.new
     @book = Book.find(params[:id])
+    @user = current_user
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
@@ -33,16 +38,21 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
-
+  def correct_user
+    @book = Book.find(params[:id])
+    if @book.user_id != current_user.id
+      redirect_to books_path
+    end
+  end
 end
